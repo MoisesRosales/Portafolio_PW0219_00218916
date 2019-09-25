ec65a022d0a0ae96f8fa273bce1185dfd4109b23
MoisesRosales <00218916@uca.edu.sv>
2019-09-25 11:04:42 -0600

    Fix ejercicio 8

--- Laboratorios/Laboratorio 5/Ejer8.js
+++ Laboratorios/Laboratorio 5/Ejer8.js
@@ -1,11 +1,14 @@
 var array=[];
 function Binary(number){
     var residuo;
-    if(number!=2){
+    if(number>=2){
         residuo = number%2;
         number = Math.floor(number/2);
         Binary(number);
         array.push(residuo);
+    }
+    else{
+        array.push(1);
     }
     console.log(array);
 }
